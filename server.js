0593b892a7799f3ef83052da8b31eca22f0372c8
Sabah Syed <sabisyed@gmail.com>
2020-07-08 01:33:26 -0700

    Minor changes, offline functionality still not working

--- server.js
+++ server.js
@@ -3,17 +3,16 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(logger("dev"));
 
 app.use(compression());
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
